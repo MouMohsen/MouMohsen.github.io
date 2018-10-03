a3ba897853da0e22630d84e9e37e08ce85648ed3
Muhammed Mohsen <muhammedmohsen@kernel.blue>
2018-10-03 11:23:32 +0200

    Minify assets & add resume

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,21 @@
-$(window).load(function() {
-  $('#overlay').fadeOut();
+new fullpage('#fullpage', {
+  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  anchors: ['home', 'projects', 'services', 'resume', 'contact'],
+  sectionsColor: ['#393F42', '#000', '#393F42', '#FFA713', '#000'],
+  menu: '#myMenu',
+  scrollingSpeed: 650,
+  autoScrolling: true,
+  scrollHorizontally: true,
+  loopBottom: true,
+  scrollOverflow: true,
+  fixedElements: '#next',
+  menu: '#menu',
+  css3: true
 });
 
-const wrapIt = item => {
-  return `<a class="fab fa-fw fa-${item}"></a>`;
-};
+$(window).on('load', () => $('#overlay').fadeOut());
+$('#next').on('click', () => fullpage_api.moveSectionDown());
+const wrapIt = item => `<a class="fab fa-fw fa-${item}"></a>`;
 
 Handlebars.registerHelper('splitTech', tech => {
   let techArray = tech.split(',');
@@ -33,7 +44,7 @@ $.get('https://boiling-chamber-41113.herokuapp.com/projects', projects => {
     if (card.hasClass('mc-active')) {
       card.removeClass('mc-active');
 
-      window.setTimeout(function() {
+      window.setTimeout(() => {
         icon
           .removeClass('fa-arrow-left')
           .removeClass('fa-spin-fast')
@@ -42,7 +53,7 @@ $.get('https://boiling-chamber-41113.herokuapp.com/projects', projects => {
     } else {
       card.addClass('mc-active');
 
-      window.setTimeout(function() {
+      window.setTimeout(() => {
         icon
           .removeClass('fa-bars')
           .removeClass('fa-spin-fast')
@@ -52,89 +63,17 @@ $.get('https://boiling-chamber-41113.herokuapp.com/projects', projects => {
   });
 });
 
-// let current = document.getElementsByClassName('menu-item active')[0].innerHTML;
+$('.header').on(
+  'mouseover',
+  () => (document.getElementById('sidebar-menu').style.left = '0')
+);
 
-$('.header').on('mouseover', function() {
-  document.getElementById('sidebar-menu').style.left = '0';
-  // document.getElementsByClassName('menu-item active')[0].innerHTML = 'Menu';
-});
+$('.header').on(
+  'mouseout',
+  () => (document.getElementById('sidebar-menu').style.left = '-320px')
+);
 
-$('.header').on('mouseout', function() {
-  document.getElementById('sidebar-menu').style.left = '-320px';
-  // document.getElementsByClassName('menu-item active')[0].innerHTML = current;
-});
-
-$('.close-btn').on('click', function() {
-  document.getElementById('sidebar-menu').style.left = '-320px';
-  // document.getElementsByClassName('menu-item active')[0].innerHTML = current;
-});
-
-$(document).ready(function() {});
-// $('.logo-icon').on('click', function() {
-//   document.getElementById('sidebar-menu').style.left = '-320px';
-//   // document.getElementsByClassName('menu-item active')[0].innerHTML = current;
-// });
-
-// var request = new XMLHttpRequest();
-// request.open('GET', 'http://localhost:3000/projects', true);
-// request.onload = function() {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response);
-// };
-// request.send();
-
-// var data = [
-//   {
-//     Category: 'General',
-//     DocumentList: [
-//       {
-//         DocumentName: 'Document Name 1 - General',
-//         DocumentLocation: 'Document Location 1 - General'
-//       },
-//       {
-//         DocumentName: 'Document Name 2 - General',
-//         DocumentLocation: 'Document Location 2 - General'
-//       }
-//     ]
-//   },
-//   {
-//     Category: 'Unit Documents',
-//     DocumentList: [
-//       {
-//         DocumentName: 'Document Name 1 - Unit Documents',
-//         DocumentList: 'Document Location 1 - Unit Documents'
-//       }
-//     ]
-//   },
-//   {
-//     Category: 'Minutes'
-//   }
-// ];
-
-// $(function() {
-//   $('.material-card').on('click', '.mc-btn-action', function() {
-//     var card = $(this).parent('.material-card');
-//     var icon = $(this).children('i');
-//     icon.addClass('fa-spin-fast');
-//
-//     if (card.hasClass('mc-active')) {
-//       card.removeClass('mc-active');
-//
-//       window.setTimeout(function() {
-//         icon
-//           .removeClass('fa-arrow-left')
-//           .removeClass('fa-spin-fast')
-//           .addClass('fa-bars');
-//       }, 800);
-//     } else {
-//       card.addClass('mc-active');
-//
-//       window.setTimeout(function() {
-//         icon
-//           .removeClass('fa-bars')
-//           .removeClass('fa-spin-fast')
-//           .addClass('fa-arrow-left');
-//       }, 800);
-//     }
-//   });
-// });
+$('.close-btn').on(
+  'click',
+  () => (document.getElementById('sidebar-menu').style.left = '-320px')
+);
